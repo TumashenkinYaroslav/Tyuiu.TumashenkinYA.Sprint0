15b5594dd34bef220c5215dc92c93445b5418129
Тумашенкин Ярослав <ariadnagileva@list.ru>
2025-09-14 13:12:42 +0500

    Task4

---
+++ Tyuiu.TumashenkinYA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.TumashenkinYA.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.TumashenkinYA.Sprint0.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.TumashenkinYA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.TumashenkinYA.Sprint0.Task4.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicatonValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
