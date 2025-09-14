0ebd6f07121bc67bff8fd9f0c92cee14b64ef3e3
Тумашенкин Ярослав <ariadnagileva@list.ru>
2025-09-15 01:24:00 +0500

    Task7

---
+++ Tyuiu.TumashenkinYA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Tyuiu.TumashenkinYA.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resualArray = new int[5];
+            for (var i = 0; i < resualArray.Length; i++)
+            {
+                resualArray[i] = numOne[i] + numTwo[i];
+            }
+            return resualArray;
+        }
+    }
+}
